466485f6e67f130a9c3da9979a97912775e241bb
Charles McCauley <charles.mccauley@shopify.com>
2019-12-13 11:10:06 -0500

    gg

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 //  * serve static messages
 //  * read GET and POST parameters
 //  * handle missing pages / 404s
-//
+// gateway 10.11.128.1
 
 
 #include <Arduino.h>
@@ -11,27 +11,39 @@
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 
-// #include <Wire.h>
-// #include <LiquidCrystal.h>
+#include <Wire.h>
+#include <LiquidCrystal.h>
 
-// #define   RS     5 // GPIO5 D1
-// #define   ENABLE 4 // GPIO4 D2
-// #define   DATA1  14 // GPIO14 D5
-// #define   DATA2  12 // GPIO12 D6
-// #define   DATA3  13 // GPIO13 D7
-// #define   DATA4  15 // GPIO15 D8
+#define   RS     5 // GPIO5 D1
+#define   ENABLE 4 // GPIO4 D2
+#define   DATA1  14 // GPIO14 D5
+#define   DATA2  12 // GPIO12 D6
+#define   DATA3  13 // GPIO13 D7
+#define   DATA4  15 // GPIO15 D8
 
-//LiquidCrystal lcd(RS, ENABLE, DATA1, DATA2, DATA3, DATA4);
+struct LCDData {
+    int cups_left = 4;
+    String line1 = "Starting";
+    String line2 = "Great software";
+};
+
+LCDData data;
+
+LiquidCrystal lcd(RS, ENABLE, DATA1, DATA2, DATA3, DATA4);
 
 
 AsyncWebServer server(80);
 
-//void writeToLcd(String message);
+void writeToLcd(LCDData data);
+void take();
+void release();
 
 const char* ssid = "Shopify";
 const char* password = "tophat made from dollar bills";
 
 const char* PARAM_MESSAGE = "message";
+
+
 
 void notFound(AsyncWebServerRequest *request) {
     request->send(404, "text/plain", "Not found");
@@ -51,14 +63,12 @@ void setup() {
     Serial.println(WiFi.localIP());
 
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        Serial.print("request yolo swag");
+        writeToLcd(data);
         request->send(200, "text/plain", "Hello, world");
     });
 
     // Send a GET request to <IP>/get?message=<message>
     server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
-                Serial.print("request yolo swag getttt");
-
         String message;
         if (request->hasParam(PARAM_MESSAGE)) {
             message = request->getParam(PARAM_MESSAGE)->value();
@@ -69,12 +79,42 @@ void setup() {
     });
 
     // Send a POST request to <IP>/post with a form field message set to <message>
-    server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request){
-                Serial.print("request yolo postttt");
+    server.on("/post", HTTP_GET, [](AsyncWebServerRequest *request){
+        String message = "Error";
 
+        Serial.println("Received a post!");
+
+        if (request->hasParam("line1")) {
+            message = request->getParam("line1")->value();
+            data.line1 = message;
+        } 
+
+        if (request->hasParam("line2")) {
+            message = request->getParam("line2")->value();
+            data.line2 = message;
+        } 
+
+        writeToLcd(data);
+
+        request->send(200, "text/plain", "Hello, POST: " + message);
+    });
+
+    server.on("/slack", HTTP_POST, [](AsyncWebServerRequest *request){
+        String message = "Missing challenge";
+        if (request->hasParam("challenge", true)) {
+            message = request->getParam("challenge", true)->value();
+            data.line2 = message;
+            writeToLcd(data);
+        } 
+        Serial.println("Received a post! with message" + message);
+        request->send(200, "text/plain", message);
+    });
+
+    server.on("/release", HTTP_GET, [](AsyncWebServerRequest *request){
         String message;
         if (request->hasParam(PARAM_MESSAGE, true)) {
             message = request->getParam(PARAM_MESSAGE, true)->value();
+            writeToLcd(data);
         } else {
             message = "No message sent";
         }
@@ -82,23 +122,28 @@ void setup() {
     });
 
     server.onNotFound(notFound);
-            Serial.print("begin yo");
 
     server.begin();
-        Serial.print("begin end");
 
-    //lcd.begin(16,2);               // initialize the lcd 
-    
-
+    lcd.begin(16,2);               // initialize the lcd 
+    writeToLcd(data);
 }
 
-// void writeToLcd(String message) {
-//     lcd.home ();                   // go home
-//     lcd.print("Hello, CHEMEX");  
-//     lcd.setCursor ( 0, 1 );        // go to the next line
-//     lcd.print (message);  
-//     delay(1000);
-// }
+void writeToLcd(LCDData data) {
+    lcd.clear();
+    lcd.home ();                   // go home
+    lcd.print(data.line1);  
+    lcd.setCursor ( 0, 1 );        // go to the next line
+    lcd.print (data.line2);  
+}
+
+void take(LCDData data){
+    data.cups_left--;
+}
+
+void release(LCDData data){
+    data.cups_left++;
+}
 
 void loop() {
   //writeToLcd("yo");
